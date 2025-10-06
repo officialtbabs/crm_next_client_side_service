b95df6b6de370b912f0aadf75df7852e4bdbea31
official-tbabs <victorotbabs@gmail.com>
2025-10-06 21:31:40 +0100

    Fix to mismatch of modals

--- src/app/customers/page.tsx
+++ src/app/customers/page.tsx
@@ -24,7 +24,7 @@ const CustomersPage = () => {
 
       switch (action) {
         case CustomerTableActions.createJob:
-          setIsCustomerFormDrawerOpened(true);
+          setIsJobFormDrawerOpened(true);
           break;
       }
     }
